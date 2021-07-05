09c40fee051619b33f5d7bcdc2864670893a4582
samir patil <samirspatil742099@gmail.com>
2021-07-05 20:13:10 +0530

    minor bug fixes in binary tree deseriazation

--- easyBT/test_.py
+++ easyBT/test_.py
@@ -1,9 +1,10 @@
 from binarytree import BinaryTree  
  
 bt=BinaryTree()
-x=[1,2,3,4,5,6]
+# x=[1,2,3,4,5,6]
+x=[1,2,None,4,5,6]
 root=bt.DesializeTree(x)
-# print(bt.InOrderTraversal(root))
+print(bt.InOrderTraversal(root))
 # print(bt.PreOrderTraversal(root))
 # print(bt.SerializeTree(root))
 # print(bt.InOrderTraversal(root))
